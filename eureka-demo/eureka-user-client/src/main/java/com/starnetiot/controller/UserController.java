93b4044614ab57f0fea966ba485c911f94a4f55a
Administrator <chenf.af@star-net.cn>
2020-05-18 10:26:07 +0800

    FEATURE  手写 Ribbon

---
+++ eureka-demo/eureka-user-client/src/main/java/com/starnetiot/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.starnetiot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: springcloud-demo
+ * @description: 类功能描述
+ * @author: Mr.CF
+ * @create: 2020-05-18 10:10:10
+ */
+@Controller
+public class UserController {
+
+    @ResponseBody
+    @GetMapping(value = "/user/get")
+    private String get(){
+        return "测试用户";
+    }
+
+}
